b5350d5c2bf7bfe4ee94797db13966599fd44970
Rgghgh <Tomer1091@gmail.com>
2016-05-23 22:05:49 +0300

    Removed notice message added github link to "about"

--- about.php
+++ about.php
@@ -3,10 +3,10 @@
 
 <div class="card">
 	<h3><?php echo $tittle; ?></h3>
-	
+
 	<p>
 		<b>What is this?</b> Mute Pack Generator creates a custom resource pack that will mute the sounds you choose to mute.
-		For example, You can choose to mute the villager sounds, or nether portal sounds. <br> 
+		For example, You can choose to mute the villager sounds, or nether portal sounds. <br>
 		<br>
 		<b>How does it work?</b> Select the sounds you want to mute, Click "Download", Set the downloaded pack as your resource pack,
 		Then play and enjoy the silence!<br>
@@ -14,19 +14,21 @@
 		<b>Can I keep using the resource packs I use now?</b> Yes! Mute Pack Generator packs contain sound files only. They do not change any textures.
 		This means you can use other resource packs together with the mute pack.<br>
 		<br>
-		<b>How the app Works?</b> PHP, MySQL, Javascript (jQuery).
+		<b>How the app Works?</b> PHP, MySQL, Javascript (jQuery).<br>
+		<br>
+        <b>Source?</b> available on <a href="https://github.com/Rgghgh/Mute-Pack-Generator" target="_blank">GitHub</a>.
 	</p>
-	
+
 	<br><br>
-	
+
 	<p class="center">
 		<b>Credits</b><br />
 		<a href="https://minecraft.net/" target="_blank">Minecraft</a> by Mojang AB,<br />
 		<a href="https://www.google.com/design/icons/" target="_blank">Material Design</a> - Icons, <br />
-		<a href="http://morrisjs.github.io/morris.js/" target="_blank">Morris JS</a> - Statistics Charts, <br />		
+		<a href="http://morrisjs.github.io/morris.js/" target="_blank">Morris JS</a> - Statistics Charts, <br />
 		<br>App Created by Rgghgh.<br />
 	</p>
-	
+
 </div>
 
 <?php include '_footer.php'; ?>
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php $tittle = "App" ?>
 <?php include '_header.php'; ?>
 
-<?php 
-	#if(isset($_GET['import']))
-	#	include '_import.php';
-?>
-
 <?php if (! isset ( $_COOKIE ['first'] ) || isset($_GET['t']) ) : include '_tutorial.php'; endif; ?>
-
-<div class="notice">
-	<b>Broken in Minecraft 1.9:</b><br>
-	Resource packs and sounds have changed and the generator was not updated.<br>
-	I do not have time to look into the new resource pack system and learn it but,<br>
-	You are more than welcome to contribute (PHP, HTML, XML, jQuery) - <a href="https://github.com/Rgghgh/Mute-Pack-Generator" target="_blank">GitHub</a>.
-</div>
 
 <form method="post" onsubmit="return valGen();" action="_generator.php">
 	
